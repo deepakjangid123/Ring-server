1a1ba78106d5e6aa1a4097913d8a24bcaae4faa0
Abhishek B Jangid <deepakjangid123@gmail.com>
2018-01-31 15:43:32 +0530

    :white_check_mark: Add more testcases

--- test/ring_server/core_test.clj
+++ test/ring_server/core_test.clj
@@ -1,11 +1,14 @@
 (ns ring-server.core-test
   "Test cases"
   (:require [clojure.test :refer :all]
-            [ring-server.core :refer :all]
+            [ring-server.core :refer [test-API parameters]]
             [clojure.spec.gen.alpha :as gen]
             [ring-server.spec :as sp]
             [ring-server.check :refer :all]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [clojure.test.check :as tc]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.generators :as gn]))
 
 (def testcase-params
   (gen/sample
@@ -29,3 +32,24 @@
   (testing "Exception"
     (is (= 500
            (:status (test-API {:params {"a" nil "b" "ASA" "c" "asd" "d" "po"}}))))))
+
+(def email-gen
+  (gn/fmap (fn [[name domain tld]]
+             (str name "@" domain "." tld))
+          (gn/tuple (gn/not-empty gn/string-alphanumeric)
+                    (gn/not-empty gn/string-alphanumeric)
+                    (gn/not-empty gn/string-alphanumeric))))
+
+(tc/quick-check 100 email-gen)
+
+(def sort-idempotent-prop
+  (prop/for-all [v (gn/vector gn/int)]
+    (= (sort v) (sort (sort v)))))
+
+(def prop-sorted-first-less-than-last
+  (prop/for-all [v (gn/not-empty (gn/vector gn/int))]
+    (let [s (sort v)]
+      (< (first s) (last s)))))
+
+(tc/quick-check 100 sort-idempotent-prop)
+(tc/quick-check 100 prop-sorted-first-less-than-last)
