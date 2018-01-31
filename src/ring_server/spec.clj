243bcae7abc080560b884cd7f4ab5229799d8648
Abhishek B Jangid <deepakjangid123@gmail.com>
2018-01-31 15:42:57 +0530

    :art: Use nilable instead of or

--- src/ring_server/spec.clj
+++ src/ring_server/spec.clj
@@ -3,8 +3,8 @@
   (:require [clojure.spec.alpha :as s]))
 
 ;; Defining all the specs
-(s/def ::coll-of-strings (s/or :coll (s/coll-of string?) :nil nil?))
-(s/def ::name string?)
-(s/def ::score int?)
-(s/def ::number number?)
-(s/def ::boolean boolean?)
+(s/def ::coll-of-strings (s/nilable (s/coll-of string?))) ;; Either collection of strings or nil
+(s/def ::name string?) ;; String else according to spec parameter does't follow the criteria, is thrown
+(s/def ::score int?) ;; Int
+(s/def ::number number?) ;; Number
+(s/def ::boolean boolean?) ;; Boolean
